a7c35870aa09f08e8f7019e597721380988c4a56
RussellGerhard <russellgerhard1@gmail.com>
2022-05-10 11:07:16 -0400

    Create skeleton files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
+var dotenv = require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var catalogRouter = require("./routes/catalog");
